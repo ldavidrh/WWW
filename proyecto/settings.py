309683009bb6d5943aa903c3f26b5c1f1f4ee43f
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-11-20 12:04:49 -0500

    change Django project to Django Rest Framework project

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
