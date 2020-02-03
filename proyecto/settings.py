e496429e18b5e7bd371491938ecca121312b6a11
Cristina <cristina.mejia@correounivalle.edu.co>
2020-02-03 17:00:52 -0500

    Listar empleados

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -149,8 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_collected')
+
+LOGIN_REDIRECT_URL = 'usuarios:home'
+LOGIN_URL = 'usuarios:login'
