ea4f0ae8b2dc5618081984e5aba7943ba8df00c0
seibern <melissa.fuentes@correounivalle.edu.co>
2020-05-18 21:50:12 -0500

    cambios en los templates base y los de usuario, se agregan los tags necesarios para la traduccion

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from apps.Usuarios import views
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('usuarios/', include('apps.Usuarios.urls', namespace='usuarios')),
     path('api/', include('apps.api.urls', namespace='api')),
