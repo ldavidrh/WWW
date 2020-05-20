72adcbc4c70663dff42e6e56378c96dd4db90af0
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2020-05-19 21:59:55 -0500

    Method for sending mails now implemented and documented

--- apps/mail/send.py
+++ apps/mail/send.py
@@ -2,12 +2,36 @@ from django.core.mail import EmailMessage
 from apps.Usuarios.models import Clientes
 from django.conf import settings
 
-email = EmailMessage(
-    subject="Tu factura electronica",
-    body="Prueba de email",
-    from_email=settings.EMAIL_HOST_USER,
-    to=["luisrestrepo1995@gmail.com"]
-)
+email = None
 
-def sendnow():
-    email.send(fail_silently=False)
+
+def setemail(recipients=[]):
+    """Recibe un array de Strings con los correos de los destinatarios y crea una instancia de correo electronico"""
+    if len(recipients) != 0:
+        email = EmailMessage(
+            subject="Tu factura electronica",
+            body="",
+            from_email=settings.EMAIL_HOST_USER,
+            to=recipient
+        )
+    else:
+        print("El array de destinatarios no puede ser vacio")
+
+
+def attach(pathtofile=''):
+    """Recibe la ruta relativa del archivo a ser adjuntado"""
+    if email == None:
+        print("Es necesario crear el Email con el metodo setmail() primero")
+    else:
+        if pathtofile == '':
+            print("Especifique la ruta del archivo a adjuntar")
+        else:
+            email.attach_file(pathtofile)
+
+
+def send_email():
+    """Envia el correo electronico, tome en cuenta que el envio puede demorar hasta 10 segundos"""
+    if email == None:
+        print("Es necesario crear el Email con el metodo setmail() primero")
+    else:
+        email.send(fail_silently=False)
