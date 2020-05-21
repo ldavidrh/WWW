80726eda85203bb0a35f50997b1cffa07ed52ff9
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2020-05-21 16:30:28 -0500

    static configuration

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -191,6 +191,6 @@ EMAIL_PORT = 587
 
 
 #Configuration regarding Heroku configuration
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
