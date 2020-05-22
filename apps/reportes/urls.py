64ec9c683f9d6688a75a680bb63d3298c117c859
Cristina <cristina.mejia@correounivalle.edu.co>
2020-05-21 22:44:28 -0500

    reporte 2

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'reportes'
 urlpatterns = [
-    path('reporte1/', views.reporte1, name='reporte1')
+    path('reporte1/', views.reporte1, name='reporte1'),
+    path('reporte2Menu/', views.reporte2Menu, name='reporte2Menu'),
+    path('reporte2/<int:pk>/', views.reporte2, name='reporte2'),
 ]
